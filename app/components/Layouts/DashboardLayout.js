454eca4e4a2da65dc7f380c30a181f4d4cb9ef2b
Mir Faisal Ahmad <mirfaisal08@gmail.com>
2025-10-17 02:58:24 +0600

    feat: adjust sidebar and main layout widths for improved UI consistency

--- app/components/Layouts/DashboardLayout.js
+++ app/components/Layouts/DashboardLayout.js
@@ -8,7 +8,7 @@ export default function DashboardLayout({ children }) {
     <ProtectedRoute>
       <div className="flex min-h-screen bg-gray-50">
         <Sidebar />
-        <main className="flex-1 w-full lg:ml-[350px]">
+        <main className="flex-1 w-full lg:ml-[300px]">
           <div className="p-4 sm:p-6 lg:p-8 pt-16 lg:pt-8">{children}</div>
         </main>
       </div>
--- app/components/Layouts/Sidebar.js
+++ app/components/Layouts/Sidebar.js
@@ -72,7 +72,7 @@ export default function Sidebar() {
 
       {/* Sidebar */}
       <aside
-        className={`fixed left-0 top-0 h-screen w-[350px] bg-white border-r border-gray-200 flex flex-col z-40 transition-transform duration-300 ease-in-out ${
+        className={`fixed left-0 top-0 h-screen w-[300px] bg-white border-r border-gray-200 flex flex-col z-40 transition-transform duration-300 ease-in-out ${
           isSidebarOpen ? "translate-x-0" : "-translate-x-full lg:translate-x-0"
         }`}>
         {/* Logo/Brand */}
